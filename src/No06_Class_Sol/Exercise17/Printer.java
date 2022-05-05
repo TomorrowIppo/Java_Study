562a44a70ce082e56f2bf216f816247fc19ec75b
TomorrowIppo <bjk1624@gmail.com>
2022-05-06 04:32:18 +0900

    이것이 자바다 - 6장 Class) Done!!

---
+++ src/No06_Class_Sol/Exercise17/Printer.java
@@ -0,0 +1,16 @@
+package No06_Class_Sol.Exercise17;
+
+public class Printer {
+    public static void println(int param) {
+        System.out.println(param);
+    }
+    public static void println(boolean param) {
+        System.out.println(param);
+    }
+    public static void println(double param) {
+        System.out.println(param);
+    }
+    public static void println(String param) {
+        System.out.println(param);
+    }
+}
