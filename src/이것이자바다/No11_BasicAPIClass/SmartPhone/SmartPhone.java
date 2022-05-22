90fe5791baaf03a79dd49b979e6b40c15835e8e3
TomorrowIppo <bjk1624@gmail.com>
2022-05-23 00:17:40 +0900

    ㅏㅡㅑ

---
+++ src/이것이자바다/No11_BasicAPIClass/SmartPhone/SmartPhone.java
@@ -0,0 +1,16 @@
+package 이것이자바다.No11_BasicAPIClass.SmartPhone;
+
+public class SmartPhone {
+    private String company;
+    private String os;
+
+    public SmartPhone(String company, String os) {
+        this.company = company;
+        this.os = os;
+    }
+
+    @Override
+    public String toString() {
+        return company + ", " + os;
+    }
+}
